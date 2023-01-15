2f2a4ced3c01d53629c18cf1f76809a68cfcdd6f
Brian Orndorff <brian.orndorff@gmail.com>
2023-01-15 08:04:57 -0800

    fixing some hightlighting

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "help", "javascript", "typescript", "c", "lua", "rust", "ruby" },
+  ensure_installed = { "help", "javascript", "typescript", "c", "lua", "rust", "ruby", "vim" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
--- nvim/lua/brian/set.lua
+++ nvim/lua/brian/set.lua
@@ -12,7 +12,6 @@ vim.opt.hidden = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
