d64d7aefdfa651ed1711c3b970e5d505f3f590fa
Brian Orndorff <brian.orndorff@springhealth.com>
2023-06-09 13:34:13 -0700

    company and some viz stuff

--- emacs/init.el
+++ emacs/init.el
@@ -8,10 +8,32 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+(global-display-line-numbers-mode)
 (setq backup-directory-alist '(("." . "~/.saves")))
 (setq backup-by-copying t)
 (setq visible-bell       nil
       ring-bell-function #'ignore)
+
+;; Set font
+(set-frame-font "JetBrains Mono 13" nil t)
+
+(use-package doom-themes
+  :ensure t
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs usersn
+  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
 
 (use-package exec-path-from-shell
   :ensure t)
@@ -19,13 +41,22 @@
 (use-package magit
   :ensure t)
 
+(use-package company
+  :ensure t
+  :config
+  (global-company-mode))
+
 (use-package lsp-mode
   :ensure t)
 
 (use-package ivy
   :ensure t
   :bind (("C-x B" . ivy-switch-buffer-other-window))
-  :init (ivy-mode 1))
+  :init (ivy-mode 1)
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-height 20)
+  (setq ivy-count-format "%d/%d "))
 
 (use-package ivy-rich
   :ensure t
@@ -33,7 +64,8 @@
 
 (use-package counsel
   :ensure t
-  :after ivy)
+  :after ivy
+  :init (counsel-mode))
   
 (use-package swiper
   :ensure t
@@ -50,8 +82,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" "da75eceab6bea9298e04ce5b4b07349f8c02da305734f7c0c8c6af7b5eaa9738" "944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" default))
  '(package-selected-packages
-   '(exec-path-from-shell counsel ivy use-package magit lsp-mode eglot)))
+   '(company exec-path-from-shell counsel ivy use-package magit lsp-mode eglot)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
