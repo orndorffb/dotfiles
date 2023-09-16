69c250e9cd5914c41689088dcad394766bfa7800
Brian Orndorff <brian.orndorff@gmail.com>
2023-09-16 09:12:10 -0700

    bunch of new telescope bindings

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,3 +4,10 @@ vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 --vim.keymap.set('n', '<leader>/', builtin.current_buffer_fuzzy_find, {})
 vim.keymap.set('n', '<leader>fs', builtin.live_grep, {})
+
+vim.keymap.set('n', '<leader>gs', builtin.lsp_document_symbols, {})
+vim.keymap.set('n', '<leader>gp', builtin.lsp_workspace_symbols, {})
+vim.keymap.set('n', 'gd', builtin.lsp_definitions, {})
+vim.keymap.set('n', 'gr', builtin.lsp_references, {})
+
+vim.keymap.set('n', '<leader>c', builtin.commands, {})
