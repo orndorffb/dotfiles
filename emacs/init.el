e9c965d2992ccede00812049b90a3ece50c69713
Brian Orndorff <brian.orndorff@gmail.com>
2024-01-28 14:39:51 -0800

    moving to consult

--- emacs/init.el
+++ emacs/init.el
@@ -26,31 +26,18 @@
 (setq create-lockfiles nil)
 
 ;; Set font
-(set-frame-font "JetBrains Mono 13" nil t)
+(set-frame-font "JetBrains Mono 12" nil t)
 
 ;; Org stuff
 (add-hook 'org-mode-hook 'org-indent-mode)
 
-(use-package doom-themes
-  :ensure t
-  :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one t)
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  (doom-themes-neotree-config)
-  ;; or for treemacs usersn
-  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
-  (doom-themes-treemacs-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
-
 (use-package exec-path-from-shell
   :ensure t)
+
+(use-package catppuccin-theme
+  :ensure t
+  :config
+  (load-theme 'catppuccin t))
 
 ;; Lsp stuff
 (use-package eglot
@@ -106,30 +93,43 @@
   :ensure t
   :bind (("C-'" . avy-goto-char-timer)))
 
-(use-package ivy
-  :ensure t
-  :bind (("C-x B" . ivy-switch-buffer-other-window))
-  :init (ivy-mode 1)
-  :config
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-height 20)
-  (setq ivy-count-format "%d/%d "))
+;; Old usage for auto completion, moving to vertico/consult for now
+;; (use-package ivy
+;;   :ensure t
+;;   :bind (("C-x B" . ivy-switch-buffer-other-window))
+;;   :init (ivy-mode 1)
+;;   :config
+;;   (setq ivy-use-virtual-buffers t)
+;;   (setq ivy-height 20)
+;;   (setq ivy-count-format "%d/%d "))
 
-(use-package ivy-rich
-  :ensure t
-  :init (ivy-rich-mode 1))
+;; (use-package ivy-rich
+;;   :ensure t
+;;   :init (ivy-rich-mode 1))
 
-(use-package counsel
-  :ensure t
-  :after ivy
-  :bind (("C-c f s" . counsel-git-grep))
-  :init (counsel-mode))
+;; (use-package counsel
+;;   :ensure t
+;;   :after ivy
+;;   :bind (("C-c f s" . counsel-git-grep))
+;;   :init (counsel-mode))
   
-(use-package swiper
+;; (use-package swiper
+;;   :ensure t
+;;   :after ivy
+;;   :bind (("C-s" . swiper)
+;; 	 ("C-r" . swiper)))
+
+(use-package vertico
   :ensure t
-  :after ivy
-  :bind (("C-s" . swiper)
-	 ("C-r" . swiper)))
+  :init
+  (vertico-mode))
+
+(use-package marginalia
+  :ensure t
+  :bind (:map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+  :init
+  (marginalia-mode))
 
 ;; Get path settings from zsh
 (when (memq window-system '(mac ns x))
@@ -141,9 +141,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" "da75eceab6bea9298e04ce5b4b07349f8c02da305734f7c0c8c6af7b5eaa9738" "944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" default))
+   '("80214de566132bf2c844b9dee3ec0599f65c5a1f2d6ff21a2c8309e6e70f9242" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" "da75eceab6bea9298e04ce5b4b07349f8c02da305734f7c0c8c6af7b5eaa9738" "944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" default))
  '(package-selected-packages
-   '(expand-region company exec-path-from-shell counsel ivy use-package magit lsp-mode eglot)))
+   '(rg robe catppuccin-theme ivy-xref expand-region company exec-path-from-shell counsel ivy use-package magit lsp-mode eglot)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
