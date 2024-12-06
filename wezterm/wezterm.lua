9af0f4c7d4756b9a34830355d6697c89484b5e6c
Brian Orndorff <brian.orndorff@gmail.com>
2024-12-05 23:22:21 -0800

    trying out wezterm instead of tmux

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,5 +7,51 @@ config.color_scheme = 'NvimLight'
 config.font_size = 12
 
 config.window_decorations = "RESIZE"
+config.tab_bar_at_bottom = true
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.keys = {
+  -- splitting
+  {
+    mods   = "LEADER",
+    key    = "-",
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
+  },
+  {
+    mods   = "LEADER",
+    key    = "=",
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
+  },
+  {
+    mods = 'LEADER',
+    key = 'z',
+    action = wezterm.action.TogglePaneZoomState
+  },
+  {
+    key = '[',
+    mods = 'LEADER',
+    action = wezterm.action.ActivateCopyMode,
+  },
+  {
+    key = 'h',
+    mods = 'LEADER',
+    action = wezterm.action{ActivatePaneDirection="Left"}
+  },
+  {
+    key = 'j',
+    mods = 'LEADER',
+    action = wezterm.action{ActivatePaneDirection="Down"}
+  },
+  {
+    key = 'k',
+    mods = 'LEADER',
+    action = wezterm.action{ActivatePaneDirection="Up"}
+  },
+  {
+    key = 'l',
+    mods = 'LEADER',
+    action = wezterm.action{ActivatePaneDirection="Right1000"}
+  }
+}
+
 
 return config
