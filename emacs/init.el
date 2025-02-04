ee5cbca5641a78c9b4638d99ee331f0d5055a3d7
Brian Orndorff <brian.orndorff@springhealth.com>
2025-02-04 14:39:57 -0800

    Added in nano

--- emacs/init.el
+++ emacs/init.el
@@ -1,17 +1,177 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
-;; Basic editor settings
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(global-display-line-numbers-mode)
-(set-fringe-mode 10)
-(add-to-list 'default-frame-alist '(undecorated-round . t))
+;;--https://gist.github.com/rougier/8d5a712aa43e3cc69e7b0e325c84eab4
+;; --- Typography stack -------------------------------------------------------
+(set-face-attribute 'default nil
+                    :height 120 :weight 'light :family "Roboto Mono")
+(set-face-attribute 'bold nil :weight 'regular)
+(set-face-attribute 'bold-italic nil :weight 'regular)
+(set-display-table-slot standard-display-table 'truncation (make-glyph-code ?…))
+(set-display-table-slot standard-display-table 'wrap (make-glyph-code ?–))
 
-;;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-;;(load-theme 'kanagawa-dragon t)
+;; --- Frame / windows layout & behavior --------------------------------------
+(setq default-frame-alist
+      '((height . 44) (width  . 81) (left-fringe . 0) (right-fringe . 0)
+        (internal-border-width . 20) (vertical-scroll-bars . nil)
+        (bottom-divider-width . 0) (right-divider-width . 0)
+        (undecorated-round . t)))
+(modify-frame-parameters nil default-frame-alist)
+(setq-default pop-up-windows nil)
 
+;; --- Activate / Deactivate modes --------------------------------------------
+(tool-bar-mode -1) (menu-bar-mode -1) (blink-cursor-mode -1)
+(global-hl-line-mode 1) (global-display-line-numbers-mode 1)
+(pixel-scroll-precision-mode 1)
+
+;; --- Minimal NANO (not a real) theme ----------------------------------------
+(defface nano-default '((t)) "")   (defface nano-default-i '((t)) "")
+(defface nano-highlight '((t)) "") (defface nano-highlight-i '((t)) "")
+(defface nano-subtle '((t)) "")    (defface nano-subtle-i '((t)) "")
+(defface nano-faded '((t)) "")     (defface nano-faded-i '((t)) "")
+(defface nano-salient '((t)) "")   (defface nano-salient-i '((t)) "")
+(defface nano-popout '((t)) "")    (defface nano-popout-i '((t)) "")
+(defface nano-strong '((t)) "")    (defface nano-strong-i '((t)) "")
+(defface nano-critical '((t)) "")  (defface nano-critical-i '((t)) "")
+
+(defun nano-set-face (name &optional foreground background weight)
+  "Set NAME and NAME-i faces with given FOREGROUND, BACKGROUND and WEIGHT"
+
+  (apply #'set-face-attribute `(,name nil
+                                ,@(when foreground `(:foreground ,foreground))
+                                ,@(when background `(:background ,background))
+                                ,@(when weight `(:weight ,weight))))
+  (apply #'set-face-attribute `(,(intern (concat (symbol-name name) "-i")) nil
+                                :foreground ,(face-background 'nano-default)
+                                ,@(when foreground `(:background ,foreground))
+                                :weight regular)))
+
+(defun nano-link-face (sources faces &optional attributes)
+  "Make FACES to inherit from SOURCES faces and unspecify ATTRIBUTES."
+
+  (let ((attributes (or attributes
+                        '( :foreground :background :family :weight
+                           :height :slant :overline :underline :box))))
+    (dolist (face (seq-filter #'facep faces))
+      (dolist (attribute attributes)
+        (set-face-attribute face nil attribute 'unspecified))
+      (set-face-attribute face nil :inherit sources))))
+
+(defun nano-install-theme ()
+  "Install THEME"
+
+  (set-face-attribute 'default nil
+                      :foreground (face-foreground 'nano-default)
+                      :background (face-background 'nano-default))
+  (dolist (item '((nano-default .  (variable-pitch variable-pitch-text
+                                    fixed-pitch fixed-pitch-serif))
+                  (nano-highlight . (hl-line highlight))
+                  (nano-subtle .    (match region
+                                     lazy-highlight widget-field))
+                  (nano-faded .     (shadow
+                                     font-lock-comment-face
+                                     font-lock-doc-face
+                                     icomplete-section
+                                     completions-annotations))
+                  (nano-popout .    (warning
+                                     font-lock-string-face))
+                  (nano-salient .   (success link
+                                     help-argument-name
+                                     custom-visibility
+                                     font-lock-type-face
+                                     font-lock-keyword-face
+                                     font-lock-builtin-face
+                                     completions-common-part))
+                  (nano-strong .    (font-lock-function-name-face
+                                     font-lock-variable-name-face
+                                     icomplete-first-match
+                                     minibuffer-prompt))
+                  (nano-critical .  (error
+                                     completions-first-difference))
+                  (nano-faded-i .   (help-key-binding))
+                  (nano-default-i . (custom-button-mouse
+                                     isearch))
+                  (nano-critical-i . (isearch-fail))
+                  ((nano-subtle nano-strong) . (custom-button
+                                                icomplete-selected-match))
+                  ((nano-faded-i nano-strong) . (show-paren-match))))
+    (nano-link-face (car item) (cdr item)))
+
+  ;; Mode & header lines 
+  (set-face-attribute 'header-line nil
+                      :background 'unspecified
+                      :underline nil
+                      :box `( :line-width 1
+                              :color ,(face-background 'nano-default))
+                      :inherit 'nano-subtle)
+  (set-face-attribute 'mode-line nil
+                      :background (face-background 'default)
+                      :underline (face-foreground 'nano-faded)
+                      :height 40 :overline nil :box nil)
+  (set-face-attribute 'mode-line-inactive nil
+                      :background (face-background 'default)
+                      :underline (face-foreground 'nano-faded)
+                      :height 40 :overline nil :box nil))
+
+(defun nano-light (&rest args)
+  "NANO light theme (based on material colors)"
+
+  (interactive)
+  (nano-set-face 'nano-default "#37474F" "#FFFFFF") ;; Blue Grey / L800
+  (nano-set-face 'nano-strong "#000000" nil 'regular) ;; Black
+  (nano-set-face 'nano-highlight nil "#FAFAFA") ;; Very Light Grey
+  (nano-set-face 'nano-subtle nil "#ECEFF1") ;; Blue Grey / L50
+  (nano-set-face 'nano-faded "#90A4AE") ;; Blue Grey / L300
+  (nano-set-face 'nano-salient "#673AB7") ;; Deep Purple / L500
+  (nano-set-face 'nano-popout "#C06C55") ;; Deep Orange / L200
+  (nano-set-face 'nano-critical "#FF6F00") ;; Amber / L900
+  (nano-install-theme))
+  
+(defun nano-dark (&rest args)
+  "NANO dark theme (based on nord colors)"
+
+  (interactive)
+  (nano-set-face 'nano-default "#ECEFF4" "#2E3440") ;; Snow Storm 3 
+  (nano-set-face 'nano-strong "#ECEFF4" nil 'regular) ;; Polar Night 0
+  (nano-set-face 'nano-highlight nil "#3B4252")  ;; Polar Night 1
+  (nano-set-face 'nano-subtle nil "#434C5E") ;; Polar Night 2 
+  (nano-set-face 'nano-faded "#677691") ;; 
+  (nano-set-face 'nano-salient "#81A1C1")  ;; Frost 2
+  (nano-set-face 'nano-popout "#D08770") ;; Aurora 1
+  (nano-set-face 'nano-critical "#EBCB8B") ;; Aurora 2
+  (nano-install-theme))
+
+;; --- Header & mode lines ----------------------------------------------------
+(setq-default mode-line-format "")
+(setq-default header-line-format
+  '(:eval
+    (let ((prefix (cond (buffer-read-only     '("RO" . nano-default-i))
+                        ((buffer-modified-p)  '("**" . nano-critical-i))
+                        (t                    '("RW" . nano-faded-i))))
+          (mode (concat "(" (downcase (cond ((consp mode-name) (car mode-name))
+                                            ((stringp mode-name) mode-name)
+                                            (t "unknow")))
+                        " mode)"))
+          (coords (format-mode-line "%c:%l ")))
+      (list
+       (propertize " " 'face (cdr prefix)  'display '(raise -0.25))
+       (propertize (car prefix) 'face (cdr prefix))
+       (propertize " " 'face (cdr prefix) 'display '(raise +0.25))
+       (propertize (format-mode-line " %b ") 'face 'nano-strong)
+       (propertize mode 'face 'header-line)
+       (propertize " " 'display `(space :align-to (- right ,(length coords))))
+       (propertize coords 'face 'nano-faded)))))
+
+;; --- Minibuffer setup -------------------------------------------------------
+(defun nano-minibuffer--setup ()
+  (set-window-margins nil 3 0)
+  
+  (setq truncate-lines t))
+(add-hook 'minibuffer-setup-hook #'nano-minibuffer--setup)
+
+(nano-light)
+
+;;--My stuff--------------------------------------------------------------------
 
 ;; Window and buffer management
 (global-set-key (kbd "C-x |") 'split-window-horizontally)
@@ -33,7 +193,7 @@
 (setq create-lockfiles nil)
 
 ;; Set font
-(set-frame-font "JetBrains Mono 12" nil t)
+;;(set-frame-font "JetBrains Mono 12" nil t)
 
 ;; Some keybinds for basic stuff
 (global-set-key (kbd "C-c s") 'consult-ripgrep)
@@ -67,8 +227,6 @@
   (setup-term-eat "rotom-eat" "cd ~/SpringCare/rotom && clear")
   (setup-term-eat "ehr-eat" "cd ~/SpringCare/spring-ehr-api && clear"))
 
-
-
 (use-package zoom-window
   :ensure t
   :bind (("C-x C-z" . zoom-window-zoom)))
@@ -98,23 +256,6 @@
   :config
   (define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
   (define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion))
-
-
- (use-package modus-themes
-   :ensure t
-   :config
-   ;; Add all your customizations prior to loading the themes
-   (setq modus-themes-italic-constructs t
-         modus-themes-bold-constructs nil)
-
-   (setq modus-themes-vivendi-color-overrides
-      '((bg-main . "#1d2021")
-        (fg-main . "#c2c2c2")))
-
-   ;; Load the theme of your choice.
-;;   (load-theme 'modus-operandi)
-
-   (define-key global-map (kbd "<f5>") #'modus-themes-toggle))
 
 (use-package gptel
   :ensure t
@@ -177,6 +318,10 @@
 (use-package rbenv
   :ensure t
   :config (global-rbenv-mode))
+
+;; Use ruby-ts-mode instead of ruby-mode
+(when (treesit-available-p)
+  (add-to-list 'major-mode-remap-alist '(ruby-mode . ruby-ts-mode)))
 
 (use-package ruby-mode
   :ensure t
@@ -243,7 +388,6 @@
   (vertico-mode)
   (vertico-multiform-mode))
 
-					; ;; Currently disabled
 (use-package vertico-posframe
   :ensure t
   :init)
@@ -330,6 +474,7 @@
 
 (setq treesit-language-source-alist
       '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+	(ruby "https://github.com/tree-sitter/tree-sitter-ruby")
 	(cmake "https://github.com/uyha/tree-sitter-cmake")
 	(css "https://github.com/tree-sitter/tree-sitter-css")
 	(elisp "https://github.com/Wilfred/tree-sitter-elisp")
