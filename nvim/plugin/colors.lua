296ffa69b24b47b60d8cb17e8ff2723f40d5598c
Brian Orndorff <brian.orndorff@springhealth.com>
2025-11-02 22:06:10 -0800

    Auto dark/light mode

--- nvim/plugin/colors.lua
+++ nvim/plugin/colors.lua
@@ -2,7 +2,20 @@ vim.pack.add({
     { src = "https://github.com/mcauley-penney/techbase.nvim" },
     { src = "https://github.com/zenbones-theme/zenbones.nvim"},
     { src = "https://github.com/rktjmp/lush.nvim"},
-    { src = "https://github.com/catppuccin/nvim"}
+    { src = "https://github.com/catppuccin/nvim"},
+    { src = "https://github.com/f-person/auto-dark-mode.nvim"}
 })
 
 require('techbase').setup({})
+
+require('auto-dark-mode').setup({
+    update_interval = 1000,
+    set_dark_mode = function()
+        vim.api.nvim_set_option_value("background", "dark", {})
+        vim.cmd("colorscheme zenbones")
+    end,
+    set_light_mode = function()
+        vim.api.nvim_set_option_value("background", "light", {})
+        vim.cmd("colorscheme zenbones")
+    end,
+})
